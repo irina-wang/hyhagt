82e2adb55d6dcdc32f5ef15a41d1890786307a50
Irina Wang <irina.wang@outlook.com>
2025-04-04 00:00:40 -0700

    add string parser

---
+++ _helper/spliter.py
@@ -0,0 +1,4 @@
+poem = "I remember days of good chilhood. Running bare upper body in the street was must , accompanied by buddy group in the low line areas where Water could be splashed. Those were the carefree days, I still reminiscise."
+l = [word for word in poem.split(" ")]
+
+print(l)
